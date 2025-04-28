27880d1b84c4f6a3053544471731929b6b72089c
mschoeffend <marlon.schoeffend@hof-university.de>
2025-04-28 20:39:24 +0200

    added SpringBoot init files

---
+++ src/test/java/java/de/StudyShare/studyshare/StudyShareApplicationTests.java
@@ -0,0 +1,13 @@
+package java.de.StudyShare.studyshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
