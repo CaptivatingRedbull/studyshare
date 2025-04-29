03820db968c515a7b3dabfb5a622bb728079239a
mschoeffend <marlon.schoeffend@hof-university.de>
2025-04-29 08:17:36 +0200

    added webconfig added to studyshareapplication

--- src/main/java/de/StudyShare/studyshare/StudyShareApplication.java
+++ src/main/java/de/StudyShare/studyshare/StudyShareApplication.java
@@ -1,13 +1,44 @@
 package de.studyshare.studyshare;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.studyshare.studyshare.domain.Role;
+import de.studyshare.studyshare.domain.User;
+import de.studyshare.studyshare.repository.UserRepository;
+
 @SpringBootApplication
-public class StudyShareApplication {
+public class StudyShareApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(StudyShareApplication.class);
+	private final UserRepository userRepository;
+
+
+	public StudyShareApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyShareApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User user1 = new User("Max", "Mustermann", "max.mustermann@hotmail.com", "xxxxxxxxx", Role.STUDENT, "mmustermann");
+		User user2 = new User("Maria", "Db", "maria.db@hotmail.com", "xxxxxxxxx", Role.ADMIN, "mariadb");
+		userRepository.saveAll(Arrays.asList(user1, user2));
+
+		// Fetch all cars and log to console
+		for (User user : userRepository.findAll()) {
+			logger.info("FirstName: {}, LastName: {}", user.getFirstName(), user.getLastName());
+		}
+	}
+
 }
+
