6c16b3d4a06cec24226d9b86d887c87a0a278fac
mschoeffend <marlon.schoeffend@hof-university.de>
2025-05-15 10:38:21 +0200

    Update .gitlab-ci.yml file

--- backend/src/main/java/de/studyshare/studyshare/config/CorsConfig.java
+++ backend/src/main/java/de/studyshare/studyshare/config/CorsConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
   @Bean
+  @Primary
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowedOrigins(List.of("http://localhost:5173")); // your front-end origin
