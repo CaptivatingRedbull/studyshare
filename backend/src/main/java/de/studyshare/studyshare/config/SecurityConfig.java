ea59fe3ac69dee9da737d6e2866eefe4edf28a71
mschoeffend <marlon.schoeffend@hof-university.de>
2025-05-15 09:54:45 +0200

    Update .gitlab-ci.yml file

--- backend/src/main/java/de/studyshare/studyshare/config/SecurityConfig.java
+++ backend/src/main/java/de/studyshare/studyshare/config/SecurityConfig.java
@@ -30,7 +30,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests
                         -> authorizeRequests
