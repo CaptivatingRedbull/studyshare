8690616e80f0f0081daeb8c349a6b1638369b942
mschoeffend <marlon.schoeffend@hof-university.de>
2025-05-31 15:28:45 +0200

    maybe maybe maybe

--- backend/src/main/java/de/studyshare/studyshare/config/CorsConfig.java
+++ backend/src/main/java/de/studyshare/studyshare/config/CorsConfig.java
@@ -29,7 +29,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         // allow orgins, HTTP-methods, headers and credentials
-        config.setAllowedOrigins(List.of("http://192.168.67.63:5173"));
+        config.setAllowedOrigins(List.of("http://192.168.67.63:5173, http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
